a198bdd91962b6082e39f4588f3695bffd03d493
Wiking <dawweber92@gmail.com>
2022-11-09 12:43:24 +0100

    Two scripts to upload

---
+++ add_noise.py
@@ -0,0 +1,60 @@
+import os
+from os import walk
+import numpy as np
+import soundfile as sf
+from functools import reduce
+
+
+class Add_noise:
+    def __init__(self, dir_path, min_freq, max_freq):
+        self.dir_path = dir_path
+        self.min_freq = min_freq
+        self.max_freq = max_freq
+
+    def files_list(self):
+        files = []
+
+        for (dir_path, dir_names, file_names) in walk(self.dir_path):
+            for file in file_names:
+                filename_ext = ['.flac', '.wav', '.mp3']
+                if file.endswith(tuple(filename_ext)):
+                    files.append(os.path.join(dir_path, file))
+
+        return files
+
+    # band limited white noise, min and max freq with 8 different steps
+    def band_limited_noise(self, samplerate, samples):
+        t = np.linspace(0, samples / samplerate, samples)
+        freqs = np.arange(self.min_freq, self.max_freq + 1, 8)
+        phases = np.random.rand(len(freqs)) * 2 * np.pi
+        signals = [np.sin(2 * np.pi * freq * t + phase) for freq, phase in zip(freqs, phases)]
+        noise = reduce(lambda a, b: a + b, signals)
+        noise /= np.max(noise)
+        return noise
+
+    def add_noise_to_file(self):
+        files = self.files_list()
+        for file in files:
+            signal, sr = sf.read(file)
+
+            print(len(signal))
+            print(sr)
+
+            noise = self.band_limited_noise(sr, samples=len(signal))
+
+            # adjustment the noise to the maximum amplitude of signal
+            signal_noise = signal + noise * abs(signal).max()
+
+            # Write to wav the signal_noise value
+            file_name = os.path.split(file)
+            print(file_name[0])
+            print(file_name[1])
+            if not os.path.exists(file_name[0] + '/Noise/'):
+                os.makedirs(file_name[0] + '/Noise/')
+            sf.write(file_name[0] + '/Noise/' + os.path.splitext(file_name[1])[0] +
+                     '.wav', signal_noise, sr, 'PCM_24')
+
+
+add_noise = Add_noise('/Users/Wiking/Desktop/LibriSpeech', 50, 800)
+add_noise.files_list()
+add_noise.add_noise_to_file()
---
+++ notch_filter.py
@@ -0,0 +1,77 @@
+import math
+import os
+from os import walk
+import soundfile as sf
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import signal
+
+
+class NotchFiltered:
+
+    def __init__(self, dir_path, freqs):
+        self.dir_path = dir_path
+        self.freqs = freqs
+
+    def files_list(self):
+        files = []
+        for (dir_path, dir_names, file_names) in walk(self.dir_path):
+            for file in file_names:
+                filename_ext = ['.flac', '.wav', '.mp3']
+                if file.endswith(tuple(filename_ext)):
+                    files.append(os.path.join(dir_path, file))
+
+        return files
+
+    # Function to create notched filteres
+    def notch_filtered(self, notch_freq, quality_factor, sample_rate, output_signal):
+        b_notch, a_notch = signal.iirnotch(notch_freq, quality_factor, sample_rate)
+        output_signal = signal.filtfilt(b_notch, a_notch, output_signal)
+
+        return output_signal
+
+    def add_noise_to_file(self):
+        files = self.files_list()
+        freqs = self.freqs
+
+        for file in files:
+            output_signal, sr = sf.read(file)
+            original_signal, sr = sf.read(file)
+
+            print(len(output_signal))
+            print(sr)
+
+            # Creating additive gaussian white noise
+            rms = math.sqrt(np.mean(original_signal ** 2))
+            original_noise = np.random.normal(0, rms, original_signal.shape[0])
+
+            # Output signal is a sum of original signal notched where in place witch filtered frequencies
+            # is additive white gaussian noise
+            # Normalization of noise signal is create on a base of maximum amplitude of original signal
+
+            for freq in freqs:
+                notched_noise = original_noise - self.notch_filtered(freq, 5, sr, original_noise)
+                output_signal = self.notch_filtered(freq, 5, sr, output_signal)
+                output_signal = output_signal + notched_noise * abs(original_noise).max()
+
+                """
+                plt.specgram(notched_noise, Fs=sr)
+                plt.show()
+                plt.specgram(output_signal, Fs=sr)
+                plt.show()
+                """
+
+            # Write to wav the signal_noise value
+            file_name = os.path.split(file)
+            print(file_name[0])
+            print(file_name[1])
+            if not os.path.exists(file_name[0] + '/Noise/'):
+                os.makedirs(file_name[0] + '/Noise/')
+            sf.write(file_name[0] + '/Noise/' + os.path.splitext(file_name[1])[0] +
+                     '.wav', output_signal, sr, 'PCM_24')
+
+
+# Class  NotchFiltered arguments are: directory path, list of frequencies to create notched filters
+notchedFiltered = NotchFiltered('/Users/Wiking/Desktop/LibriSpeech', [500, 1000, 3000, 5000])
+notchedFiltered.files_list()
+notchedFiltered.add_noise_to_file()
