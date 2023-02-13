923dc1c453f62a24db4f1efd2f7fc835f4841c0d
Wiking <dawweber92@gmail.com>
2023-02-13 12:57:06 +0100

    Implemented new method for SNR calculation

--- babble_noise_add.py
+++ babble_noise_add.py
@@ -29,15 +29,34 @@ class BabbleAdd:
         babble_file_path = self.bubble_file_path
         babble_audio, sr = librosa.load(babble_file_path)
         snr = self.snr
-        babble_audio_RMS = np.mean(librosa.feature.rms(y=babble_audio))
+
         for file in files:
             org_audio, sr = librosa.load(file)
-            org_audio_RMS = np.mean(librosa.feature.rms(y=org_audio))
             babble_audio_cut = babble_audio[np.arange(len(org_audio) % len(babble_audio))]
-            req_RMS = math.sqrt((org_audio_RMS ** 2) / (10 ** (snr / 10)))
 
-            alfa = req_RMS / babble_audio_RMS
-            signal_noise = org_audio + alfa * babble_audio_cut
+            org_audio = org_audio.astype(np.float32)
+            babble_audio_cut = babble_audio_cut.astype(np.float32)
+
+            signal_energy = np.mean(org_audio ** 2)
+            noise_energy = np.mean(babble_audio_cut ** 2)
+            gain = np.sqrt(10.0 ** (-snr / 10) * signal_energy / noise_energy)
+
+            alfa = np.sqrt(1 / (1 + gain ** 2))
+            beta = np.sqrt(gain ** 2 / (1 + gain ** 2))
+
+            org_audio_RMS = np.mean(librosa.feature.rms(y=(alfa * org_audio)))
+
+            babble_audio_RMS = np.mean(librosa.feature.rms(y=(beta * babble_audio_cut)))
+
+            print("SNR: ", 10 * math.log10((org_audio_RMS ** 2) / (babble_audio_RMS ** 2)))
+
+            signal_noise = alfa * org_audio + beta * babble_audio_cut
+
+            """
+            plt.plot(signal_noise, color='r')
+            plt.plot(org_audio, color='b')
+            plt.show()
+            """
 
             file_name = os.path.split(file)
             if file.endswith('.wav'):
@@ -47,12 +66,13 @@ class BabbleAdd:
                          '_babble_' + str(snr) + '.wav', signal_noise, sr, 'PCM_24')
                 print('File saved: ', os.path.splitext(file_name[1])[0], '_babble_' + str(snr) + '.wav')
 
+
 # Funkcja przyjmuje wartości:
 # - ścieżka do pliku z babble_speech
 # - ścieżka do folderu gdzie znajdują się nagrania mówców
 # - wartość SNR
 
-add_babble_noise = BabbleAdd('noise_test_Babble_1.wav',
+add_babble_noise = BabbleAdd('noise_test_Babble_4.wav',
                              '/Users/Wiking/Desktop/LibriSpeech', snr=40)
 add_babble_noise.files_list()
 add_babble_noise.add_babble_noise()
